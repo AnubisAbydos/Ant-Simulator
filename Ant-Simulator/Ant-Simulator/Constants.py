3f581befb9be0674980e6469f9ce9c6b2ba4d38a
Lex Hall <lex.anubis@outlook.com>
2018-03-05 16:46:53 -0700

    Began Building GroundTiles

--- Ant-Simulator/Ant-Simulator/Constants.py
+++ Ant-Simulator/Ant-Simulator/Constants.py
@@ -1,6 +1,23 @@
 # Global Variables
+#COLORS
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+#COLORS
+
+#SCREEN AND MAP SIZES
 WIDTH = 1100
 HEIGHT = 1000
+PIXELSIZE = 10
+GRIDROWS = WIDTH / PIXELSIZE
+GRIDCOLUMNS = HEIGHT / PIXELSIZE
+#SCREEN AND MAP SIZES
 
+#GroundTile CONSTS
+#used during array construction to determine cells starting state
+startAlivePercentage = 38
+
+#variables used during simulation step to change cell states
+deathLimit = 3
+birthLimit = 4
+steps = 4
+#GroundTile CONSTS
--- Ant-Simulator/Ant-Simulator/GroundTiles.py
+++ Ant-Simulator/Ant-Simulator/GroundTiles.py
@@ -1,75 +1,86 @@
-self.tiles = [[0 for x in range(WIDTH / 10)] for y in range(HEIGHT / 10)]
-drawMapTiles()
+import pygame
+import Constants as const
+from random import *
 
+#Grid variables and 2d array
+cells = [[0 for x in range(const.GRIDROWS)] for y in range(const.GRIDCOLUMNS)]
 
-# Class containing Ground Tile
-class GroundTile(object):
-    def __init__(self, row, column, isAlive, cellPixelSize):
+#Class for holding the cell's shape and state
+class cell(object):
+    def __init__(self, row, column, isAlive):
         self.cellRect = pygame.Rect(row * cellPixelSize, column * cellPixelSize, cellPixelSize, cellPixelSize)
         self.oldIsAlive = isAlive
         self.newIsAlive = isAlive
-    def setTile(self, newIsAlive):
-        self.newIsAlive = newIsAlive
-        if self.newIsAlive == True:
+        if isAlive == True:
             self.image = pygame.image.load()
         else:
             self.image = pygame.image.load()
 
-    
-    def drawMapTiles(self):
-        tilePixelSize = 10
+#function to count and return number of living neighbors
+def countAliveNeighbors(x, y):
+    count = 0
+    #loop to each neighbor including corners
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            neighbourX = x + i
+            neighbourY = y + j
+            if (i == 0 and j == 0):
+                pass
+            elif (neighbourX < 0 or neighbourY < 0 or neighbourX >= len(cells) or neighbourY >= len(cells[0])):
+                count = count + 1
+            elif (cells[neighbourX][neighbourY].oldIsAlive):
+                count = count + 1
+    return count
 
-        #used during array construction to determine tiles starting state
-        startAlivePercentage = 38
-        steps = 2
-
-        #Construct cell array Giving random starting state
-        for column in xrange(WIDTH):
-            for row in xrange(HEIGHT):
-                if(randint(0, 100) < startAlivePercentage):
-                    self.tiles[row][column] = GroundTile(row, column, True, tilePixelSize)
+#loops through every cell in grid calls countAliveNeighbors and then updates state
+def doSimulationStep():
+    for x in range(0, r):
+        for y in range(0, c):
+            neighborsAlive = countAliveNeighbors(x, y)
+            if(cells[x][y].oldIsAlive):
+                if(neighborsAlive < deathLimit):
+                    cells[x][y].newIsAlive = False
                 else:
-                    self.tiles[row][column] = GroundTile(row, column, False, tilePixelSize)
-
-        #run simulation step specified number of times
-        for i in range(0, steps):
-            doSimulationStep()
-
-    def countAliveNeighbors(self, x, y):
-        count = 0
-        #loop to each neighbor including corners
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                neighbourX = x + i
-                neighbourY = y + j
-                if (i == 0 and j == 0):
-                    pass
-                elif (neighbourX < 0 or neighbourY < 0 or neighbourX >= len(self.tiles) or neighbourY >= len(self.tiles[0])):
-                    count = count + 1
-                elif (self.tiles[neighbourX][neighbourY].oldIsAlive):
-                    count = count + 1
-        return count
-
-    #loops through every cell in grid calls countAliveNeighbors and then updates state
-    def doSimulationStep(self):
-        #variables used during simulation step to change cell states
-        deathLimit = 3
-        birthLimit = 4
-        
-        for x in range(0, WIDTH):
-            for y in range(0, HEIGHT):
-                neighborsAlive = countAliveNeighbors(x, y)
-                if(self.tiles[x][y].oldIsAlive):
-                    if(neighborsAlive < deathLimit):
-                        self.tiles[x][y].setTile(False)
-                    else:
-                        self.tiles[x][y].setTile(True)
+                    cells[x][y].newIsAlive = True
+            else:
+                if(neighborsAlive > birthLimit):
+                    cells[x][y].newIsAlive = True
                 else:
-                    if(neighborsAlive > birthLimit):
-                        self.tiles[x][y].setTile(True)
-                    else:
-                        self.tiles[x][y].setTile(False)
-        #resets old state after all new states have been set (to prepare for next simulation)
-        for x in range(0, r):
-            for y in range(0, c):
-                self.tiles[x][y].oldIsAlive = self.tiles[x][y].newIsAlive
+                    cells[x][y].newIsAlive = False
+    #resets old state after all new states have been set (to prepare for next simulation)
+    for x in range(0, r):
+        for y in range(0, c):
+            cells[x][y].oldIsAlive = cells[x][y].newIsAlive
+
+#Construct cell array Giving random starting state
+for column in xrange(r):
+    for row in xrange(c):
+        if(randint(0, 100) < startAlivePercentage):
+            cells[row][column] = cell(row, column, True)
+        else:
+            cells[row][column] = cell(row, column, False)
+
+#run simulation step specified number of times
+for i in range(0, steps):
+    doSimulationStep()
+
+#Pygame variable setup
+
+pygame.display.init()
+#Screen size is set based on number of cells in grid multiplied by each rect size to make perfect screen size
+screen = pygame.display.set_mode((r * cellPixelSize, c * cellPixelSize))
+run = True
+
+#Game loop start
+while (run):
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+    for x in range(0, r):
+        for y in range(0, c):
+            #Draw only dead cells to create the open passable area
+            if (cells[x][y].oldIsAlive == False):
+                pygame.draw.rect(screen, white, cells[x][y].cellRect, 0)
+    pygame.display.flip()
+
+pygame.display.quit()
